a0443755c3901a4220a57de53ceb235a674546f8
Gerard Lester <lesterandoque09@gmail.com>
2024-11-05 19:53:58 +0800

    divisioncheckers

--- src/app/guesscolor/guesscolor.component.ts
+++ src/app/guesscolor/guesscolor.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-guesscolor',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './guesscolor.component.html',
   styleUrl: './guesscolor.component.css'
 })
 export class GuesscolorComponent {
+userColor: string = '';
+result: string = '';
 
+colors = [
+  'Yellow',
+  'Orange',
+  'Blue',
+  'Green',
+  'Purple',
+  'Brown',
+  'Gray',
+  'Red',
+  'Pink',
+  'White',
+  'Black'];
+
+  checkColor(): void {
+    const randomColor = this.colors[Math.floor(Math.random() * this.colors.length)];
+    if (this.userColor.toLowerCase() === randomColor) {
+      this.result = 'Congratulations! You guessed the color:!';
+    } else{
+      this.result = "Sorry, you didn't get the correct color, Please ty again!";
+    }
+  }
 }
