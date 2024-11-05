47803881f8278dd07153baa461deb758b4744f8f
Gerard Lester <lesterandoque09@gmail.com>
2024-11-05 20:06:00 +0800

    divisiontable

--- src/app/divisiontable/divisiontable.component.ts
+++ src/app/divisiontable/divisiontable.component.ts
@@ -1,12 +1,21 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-divisiontable',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,NgFor,NgIf],
   templateUrl: './divisiontable.component.html',
   styleUrl: './divisiontable.component.css'
 })
 export class DivisiontableComponent {
+num: number = 0;
+table: {num: number, result: string}[]=[];
 
+generateTable(): void {
+  this.table = [];
+  for (let i = 1; i <= 10; i++) {
+  }
+  }
 }
