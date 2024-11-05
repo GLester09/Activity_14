b9894c9e68026d5c11d1ad425a3a9482de6d33e1
Gerard Lester <lesterandoque09@gmail.com>
2024-11-05 19:28:55 +0800

    appealform

--- src/app/appealform/appealform.component.ts
+++ src/app/appealform/appealform.component.ts
@@ -1,12 +1,29 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-appealform',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor,NgIf],
   templateUrl: './appealform.component.html',
   styleUrl: './appealform.component.css'
 })
 export class AppealformComponent {
+appealForm!: FormGroup;
+appealSent = false;
 
+constructor(){
+}
+
+ngOnInit(): void {
+  this.appealForm = new FormGroup({
+    appealName: new FormControl('', Validators.required),
+    appealDescription: new FormControl('', Validators.required)
+  });
+}
+onSubmit(): void {
+  console.log('Form submitted');
+  this.appealSent =true;
+}
 }
