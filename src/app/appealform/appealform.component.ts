8ed388b3dfbdbbb001ac683a0a4d2ee0620e5726
Gerard Lester <lesterandoque09@gmail.com>
2024-11-05 19:50:23 +0800

    guesscolor

--- src/app/appealform/appealform.component.ts
+++ src/app/appealform/appealform.component.ts
@@ -1,11 +1,11 @@
-import { NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-appealform',
   standalone: true,
-  imports: [FormsModule, NgFor,NgIf],
+  imports: [FormsModule, NgFor,NgIf,CommonModule,ReactiveFormsModule],
   templateUrl: './appealform.component.html',
   styleUrl: './appealform.component.css'
 })
