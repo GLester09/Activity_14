a9b5bba386ee6f9c5d49fc3e1aac38942fec4a05
Gerard Lester <lesterandoque09@gmail.com>
2024-11-05 20:25:17 +0800

    pingchecker

--- src/app/pingchecker/pingchecker.component.ts
+++ src/app/pingchecker/pingchecker.component.ts
@@ -1,12 +1,27 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pingchecker',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf,NgFor],
   templateUrl: './pingchecker.component.html',
   styleUrl: './pingchecker.component.css'
 })
 export class PingcheckerComponent {
+ ping: number = 0;
+ result: string = '';
 
+ checkPing(): void {
+  if (this.ping <= 4) {
+    this.result = 'Your ping is very good';
+  } else if (this.ping <=20){
+    this.result = "Your ping is still in good condition";
+  } else if (this.ping <= 50) {
+    this.result = 'You may experience slight lag';
+  } else {
+    this.result = "You may experience very lag and slow internet"
+  }
+ }
 }
