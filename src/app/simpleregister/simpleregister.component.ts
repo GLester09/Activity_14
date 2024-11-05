bed6865c13af99277bb76be05293da09878abdf1
Gerard Lester <lesterandoque09@gmail.com>
2024-11-05 19:16:24 +0800

    simpleregister

--- src/app/simpleregister/simpleregister.component.ts
+++ src/app/simpleregister/simpleregister.component.ts
@@ -1,12 +1,21 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-simpleregister',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,NgIf],
   templateUrl: './simpleregister.component.html',
   styleUrl: './simpleregister.component.css'
 })
 export class SimpleregisterComponent {
+  email: string = '';
+  password: string ='';
+  isRegistered: boolean = false;
 
+  register(){
+    console.log("Registering user with email and password");
+    this.isRegistered = true;
+  }
 }
